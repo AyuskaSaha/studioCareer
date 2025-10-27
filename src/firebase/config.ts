606ed1c7a9df3e6c87015807d58129423c4c9302
Ayuska Saha <ayuska.saha26@gmail.com>
2025-10-27 06:52:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8027068200-24708",
+  "appId": "1:864696738768:web:8d37b5c87ec0c07aaecfd5",
+  "apiKey": "AIzaSyAq455s5-fsFq0vT6B_UMjNBACpVfiVBEQ",
+  "authDomain": "studio-8027068200-24708.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "864696738768"
+};
